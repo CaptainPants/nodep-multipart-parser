f6fbb1187fa7f030f35bac15e72f609e9831c4db
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2022-12-04 22:47:05 +0000

    Additional unit tests, fix to arrayRemoveWhere.

---
+++ src/internal/util/arrayFindIndex.test.ts
@@ -0,0 +1,11 @@
+
+import { expect, test, describe } from '@jest/globals';
+
+import { arrayFindIndex } from './arrayFindIndex.js';
+
+test('arrayFind', () => {
+    const data = [1, 2, 3];
+
+    expect(arrayFindIndex(data, x => x === 1)).toEqual(0);
+    expect(arrayFindIndex(data, x => x === 4)).toEqual(-1);
+});
