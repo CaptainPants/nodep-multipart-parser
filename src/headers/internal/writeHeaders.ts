3e1496fbc6a9b9361d91d12016e10583455d7376
CaptainPants <2021556+CaptainPants@users.noreply.github.com>
2022-12-25 00:06:31 +1100

    Getting there with multipart uploads - still not working though.

--- src/headers/internal/writeHeaders.ts
+++ src/headers/internal/writeHeaders.ts
@@ -2,13 +2,14 @@ import { writeStringToDataView } from "../../internal/util/writeStringToDataView
 import { Header } from "../types.js";
 
 export function writeHeaders(headers: Header[], view: DataView, offset: number): number {
+    const start = offset;
+
     for (const header of headers) {
-        offset += writeStringToDataView(header.name, view, offset);
-        offset += writeStringToDataView(':', view, offset);
-        offset += writeStringToDataView(header.value, view, offset);
-        offset += writeStringToDataView('\r\n', view, offset);
+        const headerString = header.name + ':' + header.value + '\r\n';
+        offset += writeStringToDataView(headerString, view, offset);
     }
 
     offset += writeStringToDataView('\r\n', view, offset);
-    return offset;
+
+    return offset - start;
 }
